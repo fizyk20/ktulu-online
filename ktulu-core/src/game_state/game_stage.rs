4b92305752ae0fbcd1429d8d5e42499c22153f65
Bartłomiej Kamiński <fizyk20@gmail.com>
2017-03-12 19:51:05 +0100

    implement the sequence of game stages

--- ktulu-core/src/game_state/game_stage.rs
+++ ktulu-core/src/game_state/game_stage.rs
@@ -1,4 +1,4 @@
-use characters::CharacterType;
+use characters::{CharacterType, Fraction};
 
 /// Enum representing the moment in game.
 /// When the game starts, it's `BeforeStart`. Then it transitions to `Night(0)`, then `Day(1)`,
@@ -7,20 +7,78 @@ use characters::CharacterType;
 pub enum Time {
     BeforeStart,
     Day(u8),
+    DayVoteSearch(u8),
+    DayVoteKill(u8),
     Night(u8),
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Activity {
+    Character(CharacterType),
+    FractionWake(Fraction),
+    FractionSleep(Fraction),
+}
+
+const NIGHT_0_SEQUENCE: [Activity; 12] = [Activity::Character(CharacterType::Prostitute),
+                                          Activity::Character(CharacterType::Pastor),
+                                          Activity::Character(CharacterType::Seducer),
+                                          Activity::Character(CharacterType::Sheriff),
+                                          Activity::FractionWake(Fraction::Bandits),
+                                          Activity::Character(CharacterType::Boss),
+                                          Activity::Character(CharacterType::Blackmailer),
+                                          Activity::FractionSleep(Fraction::Bandits),
+                                          Activity::FractionWake(Fraction::Indians),
+                                          Activity::FractionSleep(Fraction::Indians),
+                                          Activity::FractionWake(Fraction::Aliens),
+                                          Activity::FractionSleep(Fraction::Aliens)];
+
+const NIGHT_SEQUENCE: [Activity; 27] = [Activity::Character(CharacterType::Sheriff),
+                                        Activity::Character(CharacterType::Pastor),
+                                        Activity::Character(CharacterType::TaxCollector),
+                                        Activity::Character(CharacterType::Bodyguard),
+                                        Activity::Character(CharacterType::Gambler),
+                                        Activity::Character(CharacterType::Drunkard),
+                                        Activity::FractionWake(Fraction::Bandits),
+                                        Activity::Character(CharacterType::Boss),
+                                        Activity::Character(CharacterType::Avenger),
+                                        Activity::Character(CharacterType::Thief),
+                                        Activity::Character(CharacterType::CardShark),
+                                        Activity::FractionSleep(Fraction::Bandits),
+                                        Activity::Character(CharacterType::WitchDoctor),
+                                        Activity::FractionWake(Fraction::Indians),
+                                        Activity::Character(CharacterType::Chief),
+                                        Activity::Character(CharacterType::Warrior),
+                                        Activity::Character(CharacterType::LoneCoyote),
+                                        Activity::FractionSleep(Fraction::Indians),
+                                        Activity::Character(CharacterType::SilentFoot),
+                                        Activity::Character(CharacterType::ScopeEye),
+                                        Activity::Character(CharacterType::Witch),
+                                        Activity::FractionWake(Fraction::Aliens),
+                                        Activity::Character(CharacterType::Detector),
+                                        Activity::Character(CharacterType::MindEater),
+                                        Activity::Character(CharacterType::GreenTentacle),
+                                        Activity::Character(CharacterType::GreatAlien),
+                                        Activity::FractionSleep(Fraction::Aliens)];
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct GameStage {
     time: Time,
-    active_character: Option<CharacterType>,
+    activity: Option<Activity>,
 }
 
 impl GameStage {
     pub fn new() -> GameStage {
         GameStage {
             time: Time::BeforeStart,
-            active_character: None,
+            activity: None,
+        }
+    }
+
+    #[cfg(test)]
+    pub fn new_with_data(time: Time, activity: Option<Activity>) -> GameStage {
+        GameStage {
+            time: time,
+            activity: activity,
         }
     }
 
@@ -28,29 +86,77 @@ impl GameStage {
         self.time
     }
 
-    pub fn active_character(&self) -> Option<CharacterType> {
-        self.active_character
+    pub fn activity(&self) -> Option<Activity> {
+        self.activity
     }
 
     pub fn advance(&mut self) {
-        match (self.time, self.active_character) {
-            (Time::BeforeStart, None) => {
+        match self.time {
+            Time::BeforeStart => {
+                if self.activity != None {
+                    panic!("Invalid game stage: time={:?}, activity={:?}",
+                           self.time,
+                           self.activity);
+                }
                 self.time = Time::Night(0);
-                self.active_character = Some(CharacterType::Prostitute);
+                self.activity = Some(NIGHT_0_SEQUENCE[0]);
             }
-            (Time::Night(0), Some(CharacterType::Prostitute)) => {
-                self.active_character = Some(CharacterType::Pastor);
+            Time::Night(0) => {
+                let activity_index = NIGHT_0_SEQUENCE.iter()
+                    .position(|&x| Some(x) == self.activity);
+                if let Some(activity_index) = activity_index {
+                    if activity_index + 1 < NIGHT_0_SEQUENCE.len() {
+                        self.activity = Some(NIGHT_0_SEQUENCE[activity_index + 1]);
+                    } else {
+                        self.time = Time::Day(1);
+                        self.activity = None;
+                    }
+                } else {
+                    panic!("Invalid game stage: time={:?}, activity={:?}",
+                           self.time,
+                           self.activity);
+                }
             }
-            (Time::Night(0), Some(CharacterType::Pastor)) => {
-                self.active_character = Some(CharacterType::Sheriff);
+            Time::Night(i) => {
+                let activity_index = NIGHT_SEQUENCE.iter()
+                    .position(|&x| Some(x) == self.activity);
+                if let Some(activity_index) = activity_index {
+                    if activity_index + 1 < NIGHT_SEQUENCE.len() {
+                        self.activity = Some(NIGHT_SEQUENCE[activity_index + 1]);
+                    } else {
+                        self.time = Time::Day(i + 1);
+                        self.activity = None;
+                    }
+                } else {
+                    panic!("Invalid game stage: time={:?}, activity={:?}",
+                           self.time,
+                           self.activity);
+                }
             }
-            (Time::Night(0), Some(CharacterType::Sheriff)) => {
-                self.active_character = Some(CharacterType::Boss);
+            Time::Day(i) => {
+                if self.activity != None {
+                    panic!("Invalid game stage: time={:?}, activity={:?}",
+                           self.time,
+                           self.activity);
+                }
+                self.time = Time::DayVoteSearch(i);
             }
-            _ => {
-                panic!("Invalid game stage: (time={:?}, active_character={:?})",
-                       self.time,
-                       self.active_character)
+            Time::DayVoteSearch(i) => {
+                if self.activity != None {
+                    panic!("Invalid game stage: time={:?}, activity={:?}",
+                           self.time,
+                           self.activity);
+                }
+                self.time = Time::DayVoteKill(i);
+            }
+            Time::DayVoteKill(i) => {
+                if self.activity != None {
+                    panic!("Invalid game stage: time={:?}, activity={:?}",
+                           self.time,
+                           self.activity);
+                }
+                self.time = Time::Night(i);
+                self.activity = Some(NIGHT_SEQUENCE[0]);
             }
         }
     }
--- ktulu-core/src/game_state/tests.rs
+++ ktulu-core/src/game_state/tests.rs
@@ -1,21 +1,82 @@
-use characters::CharacterType;
-use game_state::{GameStage, Time};
+use characters::{CharacterType, Fraction};
+use game_state::{Activity, GameStage, Time};
 
 #[test]
-fn test_game_stages() {
+fn test_initial_game_stages() {
     let mut stage = GameStage::new();
     assert_eq!(stage.time(), Time::BeforeStart);
-    assert_eq!(stage.active_character(), None);
+    assert_eq!(stage.activity(), None);
     stage.advance();
     assert_eq!(stage.time(), Time::Night(0));
-    assert_eq!(stage.active_character(), Some(CharacterType::Prostitute));
+    assert_eq!(stage.activity(),
+               Some(Activity::Character(CharacterType::Prostitute)));
     stage.advance();
     assert_eq!(stage.time(), Time::Night(0));
-    assert_eq!(stage.active_character(), Some(CharacterType::Pastor));
+    assert_eq!(stage.activity(),
+               Some(Activity::Character(CharacterType::Pastor)));
     stage.advance();
     assert_eq!(stage.time(), Time::Night(0));
-    assert_eq!(stage.active_character(), Some(CharacterType::Sheriff));
+    assert_eq!(stage.activity(),
+               Some(Activity::Character(CharacterType::Seducer)));
     stage.advance();
     assert_eq!(stage.time(), Time::Night(0));
-    assert_eq!(stage.active_character(), Some(CharacterType::Boss));
+    assert_eq!(stage.activity(),
+               Some(Activity::Character(CharacterType::Sheriff)));
+}
+
+#[test]
+fn test_end_night0() {
+    let mut stage = GameStage::new_with_data(Time::Night(0),
+                                             Some(Activity::FractionSleep(Fraction::Aliens)));
+    stage.advance();
+    assert_eq!(stage.time(), Time::Day(1));
+    assert_eq!(stage.activity(), None);
+}
+
+#[test]
+fn test_end_night() {
+    let mut stage =
+        GameStage::new_with_data(Time::Night(2),
+                                 Some(Activity::Character(CharacterType::GreenTentacle)));
+    stage.advance();
+    assert_eq!(stage.time(), Time::Night(2));
+    assert_eq!(stage.activity(),
+               Some(Activity::Character(CharacterType::GreatAlien)));
+    stage.advance();
+    assert_eq!(stage.time(), Time::Night(2));
+    assert_eq!(stage.activity(),
+               Some(Activity::FractionSleep(Fraction::Aliens)));
+    stage.advance();
+    assert_eq!(stage.time(), Time::Day(3));
+    assert_eq!(stage.activity(), None);
+}
+
+#[test]
+fn test_end_day() {
+    let mut stage = GameStage::new_with_data(Time::Day(3), None);
+    stage.advance();
+    assert_eq!(stage.time(), Time::DayVoteSearch(3));
+    assert_eq!(stage.activity(), None);
+    stage.advance();
+    assert_eq!(stage.time(), Time::DayVoteKill(3));
+    assert_eq!(stage.activity(), None);
+    stage.advance();
+    assert_eq!(stage.time(), Time::Night(3));
+    assert_eq!(stage.activity(),
+               Some(Activity::Character(CharacterType::Sheriff)));
+}
+
+#[test]
+#[should_panic]
+fn test_invalid_stage_night() {
+    let mut stage = GameStage::new_with_data(Time::Night(1), None);
+    stage.advance();
+}
+
+#[test]
+#[should_panic]
+fn test_invalid_stage_day() {
+    let mut stage = GameStage::new_with_data(Time::Day(1),
+                                             Some(Activity::FractionWake(Fraction::Citizens)));
+    stage.advance();
 }
